28c0996ca03c23d258b8b3469be6b1c0e772ac4b
Felipe Silva <felipe.silva@cern.ch>
2017-03-07 18:57:00 -0300

    .

--- configs/QCD_Pt-120to170_MuEnrichedPt5.py
+++ configs/QCD_Pt-120to170_MuEnrichedPt5.py
@@ -2,7 +2,7 @@ l1NtupleFiles = [
 "root://cmseos.fnal.gov//store/user/sfonseca/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_L1summary_QCD_Pt120_170/170224_132948/0000/L1Ntuple_1.root",
 "root://cmseos.fnal.gov//store/user/sfonseca/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_L1summary_QCD_Pt120_170/170224_132948/0000/L1Ntuple_2.root",
 "root://cmseos.fnal.gov//store/user/sfonseca/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_L1summary_QCD_Pt120_170/170224_132948/0000/L1Ntuple_3.root",
-"root://cmseos.fnal.gov//store/user/sfonseca/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_L1summary_QCD_Pt120_170/170224_132948/0000/L1Ntuple_4.root
+"root://cmseos.fnal.gov//store/user/sfonseca/QCD_Pt-120to170_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_L1summary_QCD_Pt120_170/170224_132948/0000/L1Ntuple_4.root"
 ]
 
 
